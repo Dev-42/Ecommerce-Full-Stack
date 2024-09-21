16bc077697379679945d73ef9622fcb4ec5c540b
Dev Bhattacharya <devbhattacharya42@gmail.com>
2024-09-22 00:56:26 +0530

    Admin and Users parts schema created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,17 @@ import { Route, Routes } from "react-router-dom";
 import AuthLayout from "./components/auth/AuthLayout";
 import Login from "./pages/auth/Login";
 import Register from "./pages/auth/register";
+import AdminLayout from "./components/admin-veiw/AdminLayout";
+import Dashboard from "./pages/admin-view/Dashboard";
+import Products from "./pages/admin-view/Products";
+import Orders from "./pages/admin-view/Orders";
+import Features from "./pages/admin-view/Features";
+import ShoppingLayout from "./components/shopping-veiw/ShoppingLayout";
+import NotFound from "./pages/not-found/NotFound";
+import Home from "./pages/shopping-veiw/home";
+import ProductsListing from "./pages/shopping-veiw/ProductsListing";
+import Checkout from "./pages/shopping-veiw/checkout";
+import Account from "./pages/shopping-veiw/Account";
 
 const App = () => {
   return (
@@ -12,6 +23,19 @@ const App = () => {
           <Route path="login" element={<Login />}></Route>
           <Route path="register" element={<Register />}></Route>
         </Route>
+        <Route path="/admin" element={<AdminLayout />}>
+          <Route path="dashboard" element={<Dashboard />}></Route>
+          <Route path="products" element={<Products />}></Route>
+          <Route path="orders" element={<Orders />}></Route>
+          <Route path="features" element={<Features />}></Route>
+        </Route>
+        <Route path="/shop" element={<ShoppingLayout />}>
+          <Route path="home" element={<Home />}></Route>
+          <Route path="products" element={<ProductsListing />}></Route>
+          <Route path="checkout" element={<Checkout />}></Route>
+          <Route path="account" element={<Account />}></Route>
+        </Route>
+        <Route path="*" element={<NotFound />}></Route>
       </Routes>
     </div>
   );
---
+++ client/src/pages/shopping-veiw/ProductsListing.jsx
@@ -0,0 +1,11 @@
+// import React from "react";
+
+const ProductsListing = () => {
+  return (
+    <div>
+      <h1>Products Listing</h1>
+    </div>
+  );
+};
+
+export default ProductsListing;
